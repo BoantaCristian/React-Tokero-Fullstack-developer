19b6412586369cd39639aabe5a20751a8de4dce9
crist <cristi_cristi222@outlook.com>
2021-10-19 14:32:01 +0300

    Configure connection string for database Add CORS Setup ApplicationContext for Code First

--- BallanceAPI/BallanceAPI/Models/ApplicationContext.cs
+++ BallanceAPI/BallanceAPI/Models/ApplicationContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace BallanceAPI.Models
 {
-    public class ApplicationContext
+    public class ApplicationContext : DbContext  //mark this class as a context class in order inherit all the functions needed to declare db models for database
     {
+        public ApplicationContext(DbContextOptions options) : base(options) //inject in the constructor the library from the base class of Database Context
+        {
+
+        }
     }
 }
--- BallanceAPI/BallanceAPI/Startup.cs
+++ BallanceAPI/BallanceAPI/Startup.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BallanceAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -24,7 +26,9 @@ namespace BallanceAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1).AddJsonOptions(options => options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore); //solve the inconvenience of sending incomplete json objects to browser through server by adding json options
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))); //establish connection with database through a connection string taken with the help of the configuration services
+            services.AddCors(); //add cross origin resource sharing service for data transfer to pass web browser
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +40,8 @@ namespace BallanceAPI
             }
 
             app.UseMvc();
+            app.UseCors(options => options.WithOrigins("http://localhost:4200").AllowAnyHeader().AllowAnyMethod()); //specify to cors the url of the frontend and any headers and methods are allowed from that source
+
         }
     }
 }
